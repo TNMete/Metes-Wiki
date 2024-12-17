6deedea4f38e781a98f3b0f32e09d58fcddc1338
TNMete <mete.karaoglan@tn.techstarter.de>
2024-12-17 19:52:11 +0100

    up

--- calc_area.py
+++
@@ -1,9 +0,0 @@
-def calc_area(width, height):
-    return width * height
-
-
-breite = float(input("Gib die Breite des Rechtecks ein: "))
-hoehe = float(input("Gib die Höhe des Rechtecks ein: "))
-
-flaeche = calc_area(breite, hoehe)
-print(f"Die Fläche des Rechtecks beträgt: {flaeche}")
