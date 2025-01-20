769128abded11618727f1d8440cdc6c9ac599e4d
TNMete <mete.karaoglan@tn.techstarter.de>
2025-01-20 15:25:23 +0100

    x

--- testscript.py
+++ testscript.py
@@ -45,3 +45,39 @@
 
 
 # verarbeite_liste(zahlen)
+
+
+# import socket
+
+# hostname = "example.com"
+# ip_address = socket.gethostbyname(hostname)
+# print(f"Die IP-Adresse von {hostname} ist {ip_address}")
+
+# --------------------------------------------------------------------------
+
+# import os
+# import platform
+
+# host = input("gib deinen host ein: ")
+
+
+# def ping(host):
+#     param = "-n" if platform.system().lower() == "windows" else "-c"
+#     command = f"ping {param} 4 {host}"
+#     return os.system(command)
+
+
+# # host = "google.com"
+# ping(host)
+
+# --------------------------------------------------------------------------
+
+# import ipaddress
+
+# network = ipaddress.ip_network("192.168.10.0/24")
+# print(f"Netzwerk: {network}")
+# print(f"Erste IP: {network[0]}")
+# print(f"Letzte IP: {network[-1]}")
+# print(f"Anzahl nutzbarer Hosts: {network.num_addresses - 2}")
+
+# --------------------------------------------------------------------------
